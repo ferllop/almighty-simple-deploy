b5cbb163a6f69f0ee72e94660598da2db6bc37f2
Ferran Llop <contacto@ferranllop.com>
2022-04-23 18:51:44 +0200

    Prompt user to select if want to clean the project directory or not

--- install.sh
+++ install.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
+
+reinitialize(){
+    rm -rf .gitignore post-receive-git-hook install.sh README.md
+    git update-ref -d HEAD
+    git rm --cached *
+    git remote remove origin
+    echo "Git project cleaned"
+}
+
 GIT_OK="Now this git repository accepts remote pushes into current branch"
 GIT_NOT_OK="There was some error configuring this git repository to accept
 remote pushes into current branch"
@@ -9,5 +18,13 @@ HOOK_NOT_OK="There was some problem installing the git hook"
 git config receive.denyCurrentBranch updateInstead && echo $GIT_OK || echo $GIT_NOT_OK
 cp post-receive-git-hook .git/hooks/post-receive && echo $HOOK_OK || echo $HOOK_NOT_OK
 
-echo "Done."
+while true; do
+    read -p "Would you like this script to remove its files and commits? [yn]: " answer
+    case $answer in
+        [Yy] ) reinitialize; break;;
+        [Nn] ) break;;
+        * ) echo "Please answer with the letter y for yes or with the letter n for no.";;
+    esac
+done
 
+echo "Done."
