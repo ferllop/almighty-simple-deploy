1c608c1669d4560d685cb5dbad1bcfee719f77c9
Ferran Llop <contacto@ferranllop.com>
2022-04-23 17:25:49 +0200

    Avoid returning an error when not providing an APP_PORT argument

--- deploy/install.sh
+++ deploy/install.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
-if [ -z "$1" ]; then
-    echo "Please provide a port where the app must run"
-else 
-    git config receive.denyCurrentBranch updateInstead
-    cp ./post-receive-git-hook ../.git/hooks/post-receive
-fi
+git config receive.denyCurrentBranch updateInstead
+cp ./post-receive-git-hook ../.git/hooks/post-receive
