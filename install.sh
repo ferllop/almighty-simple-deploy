eb28873860b10bb110530878343134a8ce73c92c
Ferran Llop <contacto@ferranllop.com>
2022-04-23 17:57:02 +0200

    Add auto removal option

--- install.sh
+++ install.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
+THIS_FILENAME=install.sh
+GIT_HOOK_FILE=post-receive-git-hook
+
 git config receive.denyCurrentBranch updateInstead
-cp ./post-receive-git-hook ../.git/hooks/post-receive
+cp $GIT_HOOK_FILE .git/hooks/post-receive
+
+if [ "$1" = "--remove-after" ]; then
+    rm $GIT_HOOK_FILE
+    rm $THIS_FILENAME
+fi
+
